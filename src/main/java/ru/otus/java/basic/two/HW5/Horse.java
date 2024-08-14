484b93e19e9a294c002c092784b5a55420f01ba4
Иван Трофимов <triv90@yandex.ru>
2024-08-14 23:48:47 +0300

    HW-4:Homework 4

--- src/main/java/ru/otus/java/basic/two/HW5/Horse.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.java.basic.two.HW5;
-
-public class Horse extends Animal{
-    public Horse(String name, int velocityRun, int velocitySwim, int endurance) {
-        super(name, velocityRun, velocitySwim, endurance, 4);
-    }
-}
