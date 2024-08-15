c51c9a40fb26fd1678d31b59be52b20e42c2d12c
Иван Трофимов <triv90@yandex.ru>
2024-08-15 22:30:41 +0300

    HW-8:Homework 8

---
+++ src/main/java/ru/otus/java/basic/two/hm8/AppArrayDataException.java
@@ -0,0 +1,22 @@
+package ru.otus.java.basic.two.hm8;
+
+public class AppArrayDataException extends RuntimeException {
+    public AppArrayDataException() {
+    }
+
+    public AppArrayDataException(String message) {
+        super(message);
+    }
+
+    public AppArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AppArrayDataException(Throwable cause) {
+        super(cause);
+    }
+
+    public AppArrayDataException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
