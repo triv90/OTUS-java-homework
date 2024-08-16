eaf1c5ff4c3e28ccc2aa17d5c88c98bd6aa18780
Иван Трофимов <triv90@yandex.ru>
2024-08-16 20:58:04 +0300

    HW-8:Homework 8 исправление по комментарию и рафактор

--- src/main/java/ru/otus/java/basic/two/hm8/Main8.java
+++ src/main/java/ru/otus/java/basic/two/hm8/Main8.java
@@ -3,10 +3,10 @@ package ru.otus.java.basic.two.hm8;
 public class Main8 {
     public static void main(String[] args) {
         try {
-            //     massive4Size(new String[][]{{"1", "2", "3", "4", "9"}, {"5", "6", "7", "8", "10"}});
+             //    massive4Size(new String[][]{{"1", "2", "3", "4", "9"}, {"5", "6", "7", "8", "10"}});
             //massive4Size(new String[][]{{"1", "2", "3", "4", "20"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
             //       massive4Size(new String[][]{{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
-                  massive4Size(new String[][]{{"1", "e", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
+            massive4Size(new String[][]{{"1", "e", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
         } catch (AppArraySizeException e) {
             e.printStackTrace();
         } catch (AppArrayDataException e) {
@@ -16,15 +16,12 @@ public class Main8 {
 
     public static void massive4Size(String[][] sentences) {
         int sum = 0;
+
         for (int i = 0; i < sentences.length; i++) {
-            for (int j = 0; j < sentences[i].length; j++) {
-                if (sentences.length != 4 || sentences[i].length != 4) {
-                    System.out.println("Длина текущего массива " + sentences.length);
-                    throw new AppArraySizeException("Длина массива не равна 4!\n");
-                }
+            if (sentences.length != 4 || sentences[i].length != 4) {
+                System.out.println("Длина текущего массива " + sentences.length);
+                throw new AppArraySizeException("Длина массива не равна 4!\n");
             }
-        }
-        for (int i = 0; i < sentences.length; i++) {
             for (int j = 0; j < sentences[i].length; j++) {
                 try {
                     Integer.parseInt(sentences[i][j]);
